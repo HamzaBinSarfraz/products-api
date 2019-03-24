68fefa4a14cf94590efbbde94e411f88b8440b7d
hamzabinsarfraz <ameer.hamza@logicps.com>
2019-03-24 16:55:15 +0500

    some changes

--- config/routes.config.js
+++ config/routes.config.js
@@ -7,7 +7,7 @@ const path = '../src/route/';
 module.exports = (app) => {
     const fs = require('fs').readdirSync(normalize_path).forEach((file) => {
         // uncomment this for testing
-        console.log(file," ______________route loaded");
+        // console.log(file," ______________route loaded");
         require(path + file)(app);
     });
 }
